0a8b7f3f3bc7e0bf16b02d5788f1427f354d5a5f
Kartikeminence03 <146444382+Kartikeminence03@users.noreply.github.com>
2023-10-04 11:49:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import HourlyWeather from './components/HourlyWeather';
-import TimeData from './components/TimeData';
 import WeatherProvider from './components/WeatherContext';
 
 function App() {
   return (
     <>
     <WeatherProvider>
-    <TimeData/>
       <HourlyWeather/>
     </WeatherProvider>
     </>
